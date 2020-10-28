efdb57522ada0b8b4db5fa5dbfec7ec619b2daf9
Rahim Miah <73519824+Rahim169@users.noreply.github.com>
2020-10-28 10:54:58 +0600

    woocommerce in stock,out stock text change
    Change the text if you want woocommerce instock,outstock

---
+++ index.php
@@ -0,0 +1,17 @@
+
+<?php
+// Functions.php te add korte hobe
+
+add_filter( 'woocommerce_get_availability', 'wcs_custom_get_availability', 1, 2);
+function wcs_custom_get_availability( $availability, $_product ) {
+    
+    // Change In Stock Text
+    if ( $_product->is_in_stock() ) {
+        $availability['availability'] = __('Available!', 'woocommerce');
+    }
+    // Change Out of Stock Text
+    if ( ! $_product->is_in_stock() ) {
+        $availability['availability'] = __('Sold Out', 'woocommerce');
+    }
+    return $availability;
+}
